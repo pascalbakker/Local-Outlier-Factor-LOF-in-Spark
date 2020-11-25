f5f202d0cef5b4aab12c3250766e73263010b7f5
Pascal Bakker <pascalsbakker@gmail.com>
2020-11-25 14:06:38 -0500

    Need to Calculate LRD average

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -17,7 +17,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.{Vectors, Vector => Vec}
 
 object Main extends App{
-
   /*
     FOR GENERATING AND READING DATA
   */
@@ -39,10 +38,8 @@ object Main extends App{
   def generateNewData(file_path: String):
   Unit = {
     val data = generateDataset(new Random(1), 20, 2, 100) //Generate 100 rows of data of 2 columns
-    writeToDisk(data, "data/data.csv")
+    writeToDisk(data,file_path)
   }
-
-
 
   /*
     LOF FUNCTIONS
@@ -63,7 +60,7 @@ object Main extends App{
     (target._1, distances)
   }
 
-  //TODO neighborhoodRDD (dID, vector) -> (dID, Array(neighborId, dist))
+  //neighborhoodRDD (dID, vector) -> (dID, Array(neighborId, dist))
   def neighborhood(spark: SparkSession, data: RDD[(Long,Vec)], k: Int):
   RDD[(Long, Array[(Long, Double)])] = {
     // Broadcast each partition
@@ -96,22 +93,93 @@ object Main extends App{
     }.reduce(_.union(_))
     kneighbors
   }
-  // TODO kDistanceRDD (diD, Array(neighborId, dist)) -> (dID, kDist)
-  def kDistance(): Unit = Unit
 
-  // TODO neighborhoodReverseRDD
-  def neighborhoodReverse(): Unit = Unit
+  // TODO neighborhoodReverseRDD (dID, Array(neighborID, dist)) => (neighborID, Array(dId,distance))
+  def neighborhoodReverse(data: RDD[(Long,Array[(Long, Double)])]):
+  RDD[(Long,Iterable[(Long,Double)])] = {
+    val reversedRDD = data.flatMap{
+          case (dID: Long, neighborhood: Array[(Long,Double)]) => neighborhood.map{
+            case (neighborID: Long, distance: Double) => (neighborID,(dID,distance))
+          }
+    }.groupByKey()
+    reversedRDD
+  }
 
-  // TODO lrdRDD
-  def lrd(): Unit = Unit
+  // TODO kDistance (dID, Array(neighborID, dist)) => (dID, kDist)
+  def kDistance(data: RDD[(Long,Array[(Long, Double)])], k: Int):
+  RDD[(Long,Double)] =  {
+    val kDistance = data.mapPartitions(iterable => {
+        val result = iterable.map{
+          case (dID: Long, neighborhood: Array[(Long,Double)]) => (dID, neighborhood(k-1)._2)
+        }
+        result
+    })
+    kDistance
+  }
 
-  // TODO neighborAverageLrdRDD
-  def neighborAverageLrd(): Unit = Unit
+  // TODO lrd (reversedRDD, kDistanceRDD) => (dID, Double)
+  def lrd(kDistanceRDD: RDD[(Long,Double)], reverseRDD: RDD[(Long,Iterable[(Long,Double)])], k: Int):
+  RDD[(Long,Double)] = {
+    val joined = kDistanceRDD.join(reverseRDD)
+    // (dID, (kdistance, Array[(neighborID, distance)]))
+    val lrdRDD = joined.mapPartitions( iterator => {
+      val result = iterator.map{
+        case (k: Long, v: (Double, Iterable[(Long,Double)])) => {
+          val sumOfDistances = v._2.foldLeft(0d){ (accum,b) =>
+            val maxval = if(b._2 > v._1) b._2 else v._1
+            accum + maxval
+          }
+          (k, 1/(sumOfDistances/k))
+        }
+      }
+      result
+    })
+    lrdRDD
+  }
 
   // TODO lofRDD
-  def lof(): Unit = Unit
+  // dID, lrd
+  // dID, Array[(neighborID, distanceToNeighbor)]
+  /*
+  def lof(lrdRDD: RDD[(Long,Double)], reverseRDD: RDD[(Long,Iterable[(Long,Double)])]): RDD[(Long,Double)] = {
+     val joinedLeft = lrdRDD.leftOuterJoin(reverseRDD)
+     val mappedLOFFlat = joinedLeft.flatMap{
+          case (neighborID: Long, v: (Double,Option[Iterable[(Long, Double)]])) => {
+          (dID,)
+       }
+     }
 
 
+
+
+
+
+
+
+
+     val mappedLOF = joinedLeft.mapPartitions{ iterator => {
+        iterator.map{
+          case (neighborID: Long, v: (Double,Option[Iterable[(Long, Double)]])) => {
+            v._2 match {
+              case Some(x) => x.map( dID_dist => {
+                (dID_dist._1, (v._1, 1))
+              }
+              ).toArray
+              case None => None
+            }
+          }
+        }
+     }}
+     val reducedLOF  = mappedLOF.groupByKey().map{
+       case (idx: Long, iter: Iterable[(Long, Double)]) => {
+          val lrd = iter.find(_._1 == idx).get._2
+          val sum = iter.filter(_._1 != idx).map(_._2).sum
+          (idx, sum / lrd / (iter.size - 1))
+       }
+     }
+     reducedLOF
+    }
+*/
   override def main(args: Array[String]) = {
     val k = 3
     val data_location = "data/data.csv"
@@ -123,7 +191,16 @@ object Main extends App{
     val rdd_string = spark.sparkContext.textFile(data_location)
     val rdd_vector = preprocess(rdd_string)
     val kneighbors = neighborhood(spark,  rdd_vector,k)
-    kneighbors.map(_.toString()).saveAsTextFile("data/kneighbors.txt")
+    // parallel
+    val neighborhoodReverseRDD = neighborhoodReverse(kneighbors)
+    val kdistanceRDD = kDistance(kneighbors,k)
+    // Final
+    val lrdRDD = lrd(kdistanceRDD, neighborhoodReverseRDD,k)
+    //val lofRDD = lof(lrdRDD, neighborhoodReverseRDD)
+    val toStringRDD = lrdRDD.map(_.toString())
+    //lofRDD.map(_.toString())
+    //kneighbors.map(_.toString()).saveAsTextFile("data/kneighbors.txt")
+    toStringRDD.saveAsTextFile("data/lrd.txt")
     spark.sparkContext.stop()
 
   }
